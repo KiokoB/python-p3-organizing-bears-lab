fac30eb3db59e17c6ee3ad4d01c9ed79e0b663f7
Bianca Kioko <kiokobianca01@gmail.com>
2024-06-08 20:45:21 +0300

    created tables, inserted data, did some sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ "Mr.Chocolate", 10, "M", "brown", "choleric", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ "Rowdy", 20, "M", "black", "melancholic", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ "Tabitha", 30, "F", "brown", "sanguine", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ "Sergeant Brown", 60, "M", "brown", "sanguine", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ "Melissa", 25, "F", "black", "sanguine", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ "Grinch", 35, "M", "brown", "phlegmatic", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ "Wendy", 70, "F", "brown", "sanguine", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+ NULL, 30, "F", "brown", "sanguine", 1);
